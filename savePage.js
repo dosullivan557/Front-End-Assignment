b268b16f4ff8a67803611600af7683368550d483
Dan <dosullivan557@gmail.com>
2017-11-13 23:01:10 +0000

    Fixed bugs
    Fixed where nav elements were not linked correctly, and allow correct page to load when refreshed.

--- savePage.js
+++ savePage.js
@@ -6,30 +6,28 @@ function changePage(){
   for (var i = 0; i < navElems.length; i++) {
     if(navElems[i].checked){
       localStorage.page = navElems[i].value;
-      // console.log(navElems[i].value)
+      sections[i].style.display="block";
+    }
+    else {
+      sections[i].style.display="none";
     }
   }
-  // setPage();
 }
-// function setPage(){
-//   var elem = localStorage.name;
-//
-// }
+
 function setLoadPage(){
   var elem = localStorage.page;
   //Default
-  if(nameStored){
+  if(!localStorage.page){
     localStorage.page = document.getElementById("tab1");
     navElems[0].checked=true;
-    sections[i].style = "display:block;"
   }
   //checks which one was saved, and checks the corresponding radio button
   for (var i = 0; i < navElems.length; i++) {
     if(navElems[i].value===elem){
       navElems[i].checked=true;
+      sections[i].style.display = "block";
+      break;
     }
+
   }
-}
-function getLocalStoreName(){
-  return localStorage.name;
 }
