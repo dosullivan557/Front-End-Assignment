85c377d50183cc2b44730bd45a34c7f0a67b430f
Dan <dosullivan557@gmail.com>
2017-11-18 16:23:12 +0000

    Fixed naming bugs in js

--- Javascript/savePage.js
+++ Javascript/savePage.js
@@ -4,7 +4,7 @@
 // //
 var navBar = document.getElementById("navbar");
 var navElems = document.getElementsByClassName("navItems");
-var nameStored=localStorage.name;
+var pageStored=localStorage.page;
 var sections = document.getElementsByClassName("Sections");
 
 //dev vars
@@ -23,6 +23,11 @@ navBar.addEventListener("click", changePage);
 clearButton.addEventListener("click", clearSave);
 refreshButton.addEventListener("click", refresh);
 
+/* Checks the navigation bar to check which element
+*  has been checked. Using this, it will determine
+*  which section will be set to visible, and then
+*  make the remaining sections to invisible.
+*/
 function changePage(){
   for (var i = 0; i < navElems.length; i++) {
     if(navElems[i].checked){
@@ -34,25 +39,33 @@ function changePage(){
     }
   }
 }
-
+/* clears the localStorage, and refreshes the page as
+*  if the user was visiting the page for the
+*  first time
+*/
 function clearSave(){
   window.localStorage.clear();
   refresh();
 }
+//refreshes the page
 function refresh(){
   location.reload();
 }
 
+
 function setLoadPage(){
-  var elem = localStorage.page;
-  //Default
+  /* Sets the default page to load to home if there is
+  *  no page saved.
+  */
   if(!localStorage.page){
     localStorage.page = document.getElementById("tab1");
     navElems[0].checked=true;
   }
-  //checks which one was saved, and checks the corresponding radio button
+  /* Checks which one was saved, and checks the
+  *  corresponding radio button
+  */
   for (var i = 0; i < navElems.length; i++) {
-    if(navElems[i].value===elem){
+    if(navElems[i].value===pageStored){
       navElems[i].checked=true;
       sections[i].style.display = "block";
       break;
