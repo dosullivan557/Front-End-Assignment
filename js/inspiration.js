136eff02088a1a6f5795c740d0f6a8aeff520b13
Dan <dosullivan557@gmail.com>
2017-12-11 18:04:08 +0000

    changed the innerHTML so it hides when no artist is selected.

--- js/inspiration.js
+++ js/inspiration.js
@@ -6,6 +6,12 @@ function check(){
   let strUser = artist.options[artist.selectedIndex].value;
 
   console.log(strUser);
-  wikipedia(strUser);
-  flickr(strUser);
+  if(strUser!=="none"){
+    wikipedia(strUser);
+    flickr(strUser);
+  }
+  else{
+    document.getElementById("flickr").innerHTML = '' ;
+    document.getElementById("Wikipedia").innerHTML = '' ;
+  }
 }
