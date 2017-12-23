64a9c83e3564bea9ee8b819b03e8e19502051aaf
Dan <dosullivan557@gmail.com>
2017-12-23 21:34:54 +0000

    Changed header size

--- js/flickrpull.js
+++ js/flickrpull.js
@@ -5,7 +5,7 @@ function flickr(search){
                 method=flickr.photos.search& \
                 api_key=977f37251b9341ce9efd2c4457eacc6d& \
                 format=json& \
-                per_page=10& \
+                per_page=8& \
                 nojsoncallback=1& \
                 tags=";
 
@@ -13,13 +13,11 @@ function flickr(search){
     let request = new Request(url);
     fetch(request)
       .then(function (response) {
-        // console.log(`res: ${response.status}`);
         return response.json();
       })
       .then(function(data) {
         let theData = "";
         let tmp = data.photos.photo;
-        // console.log(data);
         for(let key in tmp) {
           let url = `https://farm${tmp[key].farm}.staticflickr.com/${tmp[key].server}/${tmp[key].id}_${tmp[key].secret}_q.jpg`;
           theData += `<img src="${url}" alt="${tmp[key].title}">`;
